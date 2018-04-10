541d0ee191ecd1bf4873512c123da8a4344b1446
Robin <ageruuu@gmail.com>
2018-04-10 21:28:20 +0200

    Внутренняя майнинга

--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
 export class AppComponent {
-  title = 'Welcome to Anvel!';
+	constructor(auth: AuthService) {
+window.onload = function() {
+console.log(auth.checkAuth());
+};
+		
+
+	}
 }
--- app/Http/Controllers/CloudMiningViewController.php
+++ app/Http/Controllers/CloudMiningViewController.php
@@ -10,9 +10,7 @@ use Illuminate\Http\Request;
 class CloudMiningViewController extends Controller
 {
     public function index() {
-        $user = Auth::user();
     	$mining = CloudMining::latest()->get();
-        $portfolios = UserPortfolio::where('user_id', $user->id)->get();
     	foreach ($mining as $item) {
     		$history = $item->history;
     		$got = 0;
@@ -42,39 +40,49 @@ class CloudMiningViewController extends Controller
             
     	}
     	$mining = $mining->toArray();
-    	// $mining = CloudMining::with('history')->latest()->get()->toArray();
-    	// $history = $mining->history;
-        // $news = CloudMining::latest()->get()->toArray();
         $news = array_values($mining);
-        
-        // dd( $news );
-        // foreach ($news as $item) {
-        //  array_push($item, '');
-        // }
-
-        // $main_news = Review::with('photos')->with('category')->latest()->get()->where('main', 1)->take(2)->toArray();
-        // $main_news = array_values($main_news);
-        // $news = $news->toJson();
-        // $news = array_values($news);
-        // $comments = $news->comments();
-        // $photos = $news->photos();
-        // for ($i=0; $i < count($news); $i++) { 
-        //     if($news[$i]->photos){
-        //                 foreach ($news[$i]->photos as $photo) {
-                         
-        //                 // $news[$i]['photos']['file'] = array($photo->file);   # code...
-        //                 // echo $photo->file;
-        //             }
-        //     }
-        // }
         return response()->json([
-            'news' => $news,
-            'portfolios'=>$portfolios
-            // 'main_news' => $main_news,
-            // 'photos'=>$photos
+            'news' => $news
         ]);
-        // dd($news);
-        // return $news->toJson();
+
+
+    }
+
+public function show($id) {
+        $item = CloudMining::findOrFail($id);
+        foreach ($mining as $item) {
+            $history = $item->history;
+            $got = 0;
+            foreach ($history as $h_item) {
+
+            if ($h_item->type == 2) {
+
+                $got = $got+$h_item->price;
+
+            } else {
+
+                $got = $got-$h_item->price;
+
+            }
+            }
+            $latest_history = $item->history->where('type', 2)->last();
+            if($latest_history) {
+                $latest_date = round((strtotime("now") - strtotime($latest_history->datetime))  / (60 * 60 * 24), 0);
+                $item['latest_date'] = $latest_date;
+            }
+            $depo_date = round((strtotime("now") - strtotime($item->depo_date))  / (60 * 60 * 24));
+            $item['depo_date'] = $depo_date;
+            $percentage = round($got / ($item->depo / 100),2);
+            $item['percentage'] = $percentage;
+            $start_days = round((strtotime("now") - strtotime($item->start))  / (60 * 60 * 24), 0);
+            $item['start_days'] = $start_days;
+            
+        }
+        $mining = $mining->toArray();
+        $news = array_values($mining);
+        return response()->json([
+            'news' => $news
+        ]);
 
 
     }
