87977910d672c7e4e06707c90f9bb89e18712bde
Xeror <grides192@gmail.com>
2018-04-17 09:26:58 +0300

    Подключение сервиса поиска бирж к основному проекту

---
+++ app/Http/Controllers/BitController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Админ
+ * Date: 07.04.2018
+ * Time: 8:38
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Founder\FounderEngine;
+use App\Models\SearchForm;
+use Illuminate\Support\Facades\DB;
+
+class BitController extends Controller
+{
+    private $engine;
+
+    public function __construct()
+    {
+        $this->engine = new FounderEngine();
+    }
+
+    public function index()
+    {
+        $form = new SearchForm($_GET);
+        if (!$form->validate()) {
+            return [];
+        }
+        return $this->engine->sendRequest($form);
+    }
+
+    public function pair()
+    {
+        $exchanges = DB::select("CALL getPair('".$_GET['pair']."')");
+        $response = [];
+        foreach ($exchanges as $exchange){
+            $response[$exchange->row] = $exchange->value;
+        }
+        return $response;
+    }
+}
