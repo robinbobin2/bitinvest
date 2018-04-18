eff733b79f1b0e4274d246ae7ffacff982055d1e
Xeror <grides192@gmail.com>
2018-04-18 19:16:31 +0300

    команды

---
+++ app/Console/Commands/Rabbit.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class Rabbit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rabbit:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * @var AMQPStreamConnection
+     */
+    private $connection;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $channel = $this->getConnection()->channel();
+        $channel->queue_declare('rpc_queue', false, false, false, false);
+        echo " [x] Awaiting RPC requests\n";
+
+        $channel->basic_qos(null, 1, null);
+        $channel->basic_consume('rpc_queue', '', false, false, false, false, [$this, 'callback']);
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $this->getConnection()->close();
+    }
+
+    /**
+     * @param AMQPMessage $message
+     * @return mixed
+     */
+    public function callback($message)
+    {
+        /* @var \App\Models\Founder\Models\Requests\Request $request */
+        $request = unserialize($message->body);
+        $newMessage = new AMQPMessage(
+            serialize($request->provider->{$request->getFunction()}($request)),
+            ['correlation_id' => $message->get('correlation_id')]
+        );
+
+        /** @var AMQPChannel $channel */
+        $channel = $message->delivery_info['channel'];
+        $channel->basic_publish(
+            $newMessage, '', $message->get('reply_to'));
+        $channel->basic_ack(
+            $message->delivery_info['delivery_tag']);
+
+        return $message;
+    }
+
+    /**
+     * @return AMQPStreamConnection
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * @return AMQPChannel
+     */
+    public function getChannel()
+    {
+        return $this->getConnection()->channel();
+    }
+}
---
+++ app/Console/Commands/Updater.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Админ
+ * Date: 11.04.2018
+ * Time: 0:20
+ */
+
+namespace App\Console\Commands;
+
+
+use App\Models\Founder\Models\Providers\BiBoxProvider;
+use App\Models\Founder\Models\Providers\BinanceProvider;
+use App\Models\Founder\Models\Providers\Bit2CProvider;
+use App\Models\Founder\Models\Providers\BitBankProvider;
+use App\Models\Founder\Models\Providers\BitBayProvider;
+use App\Models\Founder\Models\Providers\BitFinexProvider;
+use App\Models\Founder\Models\Providers\HitBTCProvider;
+use App\Models\Founder\Models\Providers\PoloniexProvider;
+use App\Models\Founder\Models\Rabbit\RabbitTestHandler;
+use App\Models\Founder\Models\Requests\Request;
+use App\Models\Founder\Models\Test\Bit2C;
+use App\Models\Founder\Models\Test\BitBay;
+
+class Updater extends \Illuminate\Console\Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'updater:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach ($this->suppliers as $supplier) {
+            $supplier = new $supplier;
+            $newRequest = new Request();
+            $newRequest->setProvider($supplier);
+            $this->handler->call($newRequest);
+        }
+        while (count($this->handler->test)) {
+            $this->handler->getChannel()->wait();
+        }
+        return null;
+    }
+
+    private $suppliers = [
+//        PoloniexProvider::class,
+        HitBTCProvider::class,
+        BiBoxProvider::class,
+//        BinanceProvider::class,
+//        Bit2CProvider::class,
+//        BitBayProvider::class,
+//        BitFinexProvider::class
+    ];
+
+    /**
+     * @var RabbitTestHandler
+     */
+    private $handler;
+
+    /**
+     * FounderEngine constructor.
+     */
+    public function __construct()
+    {
+        $this->handler = new RabbitTestHandler();
+        parent::__construct();
+    }
+}
