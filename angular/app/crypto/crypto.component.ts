a806aeed90a390a9e4a28f391c9781320ca850e0
Robin <ageruuu@gmail.com>
2018-05-06 20:55:16 +0200

    оптимизация

--- angular/app/crypto/crypto.component.ts
+++ angular/app/crypto/crypto.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Http } from '@angular/http';
-import { OnChanges } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { NgForm } from '@angular/forms';
-// import { interval } from 'rxjs/Observable/interval';
 import {Router, ActivatedRoute} from '@angular/router';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import {AuthService} from '../auth.service';
-import {AddCommentService} from '../add-comment.service';
 import {StocksService} from '../stocks.service';
 
 export interface CryptoData {
@@ -57,18 +53,18 @@ export class CommentRaw {
   styleUrls: ['./crypto.component.css'],
   providers: [StocksService]
 })
-export class CryptoComponent implements OnInit {
+export class CryptoComponent implements OnInit, OnDestroy {
   comments: CommentRaw[] = [];
   dataUsd: CryptoData;
   data: PositionData;
   load:boolean=true;
   user: User = {
-      id:0,
-      name: '',
-      email:'',
-      photo_id: 0,
-      role_id: 0,
-      telegram: '',
+    id:0,
+    name: '',
+    email:'',
+    photo_id: 0,
+    role_id: 0,
+    telegram: '',
 
   }; 
   commentcount = 0;
@@ -76,13 +72,11 @@ export class CryptoComponent implements OnInit {
   main_news: any;
   news: any;
   info: any;
-  obs: any;
-  resp: any;
   infoCrypto: any;
   stocksData: any;
   cryptoFirst:any;
-cryptoData: any;
-  constructor(private http:HttpClient,private stocksServise:StocksService, 
+  cryptoData: any;
+  constructor(private http:HttpClient,private stocksService:StocksService,
     private router:Router, private route:ActivatedRoute, 
     public auth: AuthService) {
   }
@@ -100,63 +94,64 @@ cryptoData: any;
       this.stocks = JSON.parse(localStorage.getItem(symbol+'USD stocks'));
       this.load = false;
 
-      }
+    }
     this.stocksData = Observable.interval(1000).take(50).concatMap(()=>
-    this.stocksServise.getStocks(symbol+'/USDT'))
+      this.stocksService.getStocks(symbol+'/USDT'))
     .map(response => {
-        this.stocks = response;
+      this.stocks = response;
       this.load = false;
-        localStorage.setItem(symbol+'USD stocks', JSON.stringify(this.stocks));
-      }).subscribe();
+      localStorage.setItem(symbol+'USD stocks', JSON.stringify(this.stocks));
+    }).subscribe();
 
 
 
 
-    this.cryptoFirst = this.stocksServise.getCrypto()
-        .map((response)=>{
-          this.dataUsd = response[symbol+'/USDT'];
-          localStorage.removeItem(symbol);
-          localStorage.setItem(symbol, JSON.stringify(this.dataUsd));
-        }).subscribe();
-    this.cryptoData=Observable.interval(1000).take(50).concatMap(()=>this.stocksServise.getCrypto())
-        .map((response)=>{
-          this.dataUsd = response[symbol+'/USDT'];
-          localStorage.removeItem(symbol);
-          localStorage.setItem(symbol, JSON.stringify(this.dataUsd));
-        }).subscribe();
+    this.cryptoFirst = this.stocksService.getCrypto()
+    .map((response)=>{
+      this.dataUsd = response[symbol+'/USDT'];
+      localStorage.removeItem(symbol);
+      localStorage.setItem(symbol, JSON.stringify(this.dataUsd));
+    }).subscribe();
     let infoCryptoPath = "/allcrypto/"+symbol;
     this.infoCrypto = this.http.get<PositionData>(infoCryptoPath).publishReplay(1).refCount();
-      this.infoCrypto.subscribe(response => {
-        this.data = response;
-        for(let item of response['comments']) {
-          this.comments.push({ 
-              id: item.id,
-              author: item.author,
-              body: item.body,
-              email: item.email,
-              commentable_id:item.commentable_id,
-              photo:item.photo
+    this.infoCrypto.subscribe(response => {
+      this.data = response;
+      for(let item of response['comments']) {
+        this.comments.push({ 
+          id: item.id,
+          author: item.author,
+          body: item.body,
+          email: item.email,
+          commentable_id:item.commentable_id,
+          photo:item.photo
 
-          });
+        });
 
-        }
+      }
       this.commentcount = response['comments_count'];
       let newsUrl = "/postsbycat/"+this.data.cat_id_news;
-        let newsInfo = this.http.get<any>(newsUrl).publishReplay(1).refCount();
-         newsInfo.subscribe(response => {
+      let newsInfo = this.http.get<any>(newsUrl).publishReplay(1).refCount();
+      newsInfo.subscribe(response => {
         this.main_news = response['main_news'];
-       this.news = response['news'];
-       console.log(this.news);
-       console.log(this.main_news);
-     });
+        this.news = response['news'];
+        console.log(this.news);
+        console.log(this.main_news);
       });
-        this.auth
-      .getUser()
-      .subscribe(
-        (response) => {
-          this.user = response;
-          this.auth.setUser(this.user);
-        }
+    });
+
+    this.cryptoData=Observable.interval(1000).take(50).concatMap(()=>this.stocksService.getCrypto())
+    .map((response)=>{
+      this.dataUsd = response[symbol+'/USDT'];
+      localStorage.removeItem(symbol);
+      localStorage.setItem(symbol, JSON.stringify(this.dataUsd));
+    }).subscribe();
+    this.auth
+    .getUser()
+    .subscribe(
+      (response) => {
+        this.user = response;
+        this.auth.setUser(this.user);
+      }
       );
   }
 
@@ -164,25 +159,25 @@ cryptoData: any;
 
     const headers = new HttpHeaders({'Content-type': 'Application/json '});
     this.http.post('/storecomment', {
-            'post_id': post_id,
-            'body': form.value.body,
-            'commentable_id': post_id,
-            'commentable_type': type
-      }, {headers: headers}).subscribe(
-        (response) => 
-        this.comments.unshift({
-            id: response['id'],
-            email:response['email'],
-          author: response['author'],
-          body: response['body'],
-          commentable_id:response['commentable_id'],
-          photo: response['photo']
-        }),
-        (error) => console.log(error)
-      );
-      form.reset();
-      console.log(form);
-      this.commentcount=this.commentcount+1;
+      'post_id': post_id,
+      'body': form.value.body,
+      'commentable_id': post_id,
+      'commentable_type': type
+    }, {headers: headers}).subscribe(
+    (response) => 
+    this.comments.unshift({
+      id: response['id'],
+      email:response['email'],
+      author: response['author'],
+      body: response['body'],
+      commentable_id:response['commentable_id'],
+      photo: response['photo']
+    }),
+    (error) => console.log(error)
+    );
+    form.reset();
+    console.log(form);
+    this.commentcount=this.commentcount+1;
   }
   isNegative(now) {
     if((parseInt(now)) >= 0) {
@@ -202,11 +197,10 @@ cryptoData: any;
     } 
     return false;
   }
-ngOnDestroy() {
+  ngOnDestroy() {
 
-  // this.infoCrypto.unsubscribe();
-  this.cryptoData.unsubscribe();
-  this.stocksData.unsubscribe();
-  this.cryptoFirst.unsubscribe();
-}
+    this.cryptoData.unsubscribe();
+    this.stocksData.unsubscribe();
+    this.cryptoFirst.unsubscribe();
+  }
 }
--- angular/app/sidebar/stocks-sidebar/stocks-sidebar.component.ts
+++ angular/app/sidebar/stocks-sidebar/stocks-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {StocksService} from '../../stocks.service';
 import { Observable } from 'rxjs/Rx';
@@ -16,47 +16,46 @@ export class Cripto {
   styleUrls: ['./stocks-sidebar.component.scss'],
   providers: [StocksService],
 })
-export class StocksSidebarComponent implements OnInit {
- dataUsd:any=[];
+
+export class StocksSidebarComponent implements OnInit, AfterViewInit, OnDestroy {
+  dataUsd:any=[];
   constructor(private http:HttpClient,
-    private stocksServise:StocksService
-    ) { }
-data: any;
-resp: any;
-cryptoData: any;
-load:boolean = true;
-  ngOnInit() {
-    const alldata = this.http.get<Array<Cripto>>('/allcrypto');
-if(localStorage.getItem('data')) {
-      this.load = false;
-      this.dataUsd = JSON.parse(localStorage.getItem('data'));
-      }
-    this.stocksServise.getCrypto()
-        .subscribe(response => {
-            this.resp = response;
-            this.data = alldata.subscribe(response => {
-      let admin = response;
-      for (var _i = 0; _i < admin.length; ++_i) {
-        let index = _i;
-        let symbol = admin[index].symbol;
-        let year = admin[index].year;
-        let algo = admin[index].algo;
-        let desc = 'DESC';
-        if(this.dataUsd[index]) {
-                this.dataUsd[index].sym = symbol;
-                this.dataUsd[index].algo = algo;
-                this.dataUsd[index].year = year;
-                this.dataUsd[index].last = this.resp[symbol+'/USDT']['last'];
-                this.dataUsd[index].now = this.resp[symbol+'/USDT']['now'];
-                this.dataUsd[index].min = this.resp[symbol+'/USDT']['min'];
-                this.dataUsd[index].max = this.resp[symbol+'/USDT']['max'];
-                this.dataUsd[index].value = this.resp[symbol+'/USDT']['value'];
-                this.dataUsd[index].day = this.resp[symbol+"/USDT"]['day'];
-                this.dataUsd[index].week = this.resp[symbol+"/USDT"]['week'];
-                
-          
-        } else {
-          this.dataUsd[index] = {
+    private stocksService:StocksService) { }
+  
+  data: any;
+  resp: any;
+  cryptoData: any;
+  load:boolean = true;
+  alldata = this.http.get<Array<Cripto>>('/allcrypto');
+
+  ngAfterViewInit() {
+
+    this.stocksService.getCrypto()
+    .subscribe(response => {
+      this.resp = response;
+      this.data = this.alldata.subscribe(response => {
+        let admin = response;
+        for (var _i = 0; _i < admin.length; ++_i) {
+          let index = _i;
+          let symbol = admin[index].symbol;
+          let year = admin[index].year;
+          let algo = admin[index].algo;
+          let desc = 'DESC';
+          if(this.dataUsd[index]) {
+            this.dataUsd[index].sym = symbol;
+            this.dataUsd[index].algo = algo;
+            this.dataUsd[index].year = year;
+            this.dataUsd[index].last = this.resp[symbol+'/USDT']['last'];
+            this.dataUsd[index].now = this.resp[symbol+'/USDT']['now'];
+            this.dataUsd[index].min = this.resp[symbol+'/USDT']['min'];
+            this.dataUsd[index].max = this.resp[symbol+'/USDT']['max'];
+            this.dataUsd[index].value = this.resp[symbol+'/USDT']['value'];
+            this.dataUsd[index].day = this.resp[symbol+"/USDT"]['day'];
+            this.dataUsd[index].week = this.resp[symbol+"/USDT"]['week'];
+
+
+          } else {
+            this.dataUsd[index] = {
               sym: '',
               last: 0,
               now: 0,
@@ -67,25 +66,25 @@ if(localStorage.getItem('data')) {
               algo: '',
               week: 0,
               day: 0,
+            }
           }
+          this.load = false;
+          localStorage.removeItem('data');
+          localStorage.setItem('data',JSON.stringify(this.dataUsd))
         }
-        this.load = false;
-        localStorage.removeItem('data');
-            localStorage.setItem('data',JSON.stringify(this.dataUsd))
-       }
+      });
     });
-          });
-    this.cryptoData=Observable.interval(1000).take(50).concatMap(()=>this.stocksServise.getCrypto())
-        .map((response)=>{this.resp = response; console.log(this.resp)}).subscribe(()=>{
-    
+    this.cryptoData=Observable.interval(1000).take(50).concatMap(()=>this.stocksService.getCrypto())
+    .map((response)=>{this.resp = response; console.log(this.resp)}).subscribe(()=>{
 
-    
-    this.data = alldata.subscribe(response => {
+
+
+      this.data = this.alldata.subscribe(response => {
 
       // console.log(response);
       let admin = response;
       for (var _i = 0; _i < admin.length; ++_i) {
-        
+
         // console.log(this.admin[i].symbol);
         let index = _i;
         let symbol = admin[index].symbol;
@@ -93,39 +92,45 @@ if(localStorage.getItem('data')) {
         let algo = admin[index].algo;
         let desc = 'DESC';
         if(this.dataUsd[index]) {
-                this.dataUsd[index].sym = symbol;
-                this.dataUsd[index].algo = algo;
-                this.dataUsd[index].year = year;
-                this.dataUsd[index].last = this.resp[symbol+'/USDT']['last'];
-                this.dataUsd[index].now = this.resp[symbol+'/USDT']['now'];
-                this.dataUsd[index].min = this.resp[symbol+'/USDT']['min'];
-                this.dataUsd[index].max = this.resp[symbol+'/USDT']['max'];
-                this.dataUsd[index].value = this.resp[symbol+'/USDT']['value'];
-                this.dataUsd[index].day = this.resp[symbol+"/USDT"]['day'];
-                this.dataUsd[index].week = this.resp[symbol+"/USDT"]['week'];
-                
+          this.dataUsd[index].sym = symbol;
+          this.dataUsd[index].algo = algo;
+          this.dataUsd[index].year = year;
+          this.dataUsd[index].last = this.resp[symbol+'/USDT']['last'];
+          this.dataUsd[index].now = this.resp[symbol+'/USDT']['now'];
+          this.dataUsd[index].min = this.resp[symbol+'/USDT']['min'];
+          this.dataUsd[index].max = this.resp[symbol+'/USDT']['max'];
+          this.dataUsd[index].value = this.resp[symbol+'/USDT']['value'];
+          this.dataUsd[index].day = this.resp[symbol+"/USDT"]['day'];
+          this.dataUsd[index].week = this.resp[symbol+"/USDT"]['week'];
+
           
         } else {
           this.dataUsd[index] = {
-              sym: '',
-              last: 0,
-              now: 0,
-              min:0,
-              max: 0,
-              value:0,
-              year: 0,
-              algo: '',
-              week: 0,
-              day: 0,
+            sym: '',
+            last: 0,
+            now: 0,
+            min:0,
+            max: 0,
+            value:0,
+            year: 0,
+            algo: '',
+            week: 0,
+            day: 0,
           }
         }
         localStorage.removeItem('data');
-            localStorage.setItem('data',JSON.stringify(this.dataUsd))
-       }
+        localStorage.setItem('data',JSON.stringify(this.dataUsd))
+      }
     });
     });
   }
-ngOnDestroy() {
+  ngOnInit() {
+    if(localStorage.getItem('data')) {
+      this.load = false;
+      this.dataUsd = JSON.parse(localStorage.getItem('data'));
+    }
+  }
+  ngOnDestroy() {
 
   // this.data.unsubscribe();
   this.cryptoData.unsubscribe();
