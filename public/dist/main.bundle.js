6fedbdb30fb10266771a93a85d14cafe0c2391bb
Robin <ageruuu@gmail.com>
2018-05-21 21:57:22 +0200

    верстка

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -6896,7 +6896,9 @@ var StocksSidebarComponent = (function () {
     };
     StocksSidebarComponent.prototype.ngOnDestroy = function () {
         // this.data.unsubscribe();
-        this.cryptoData.unsubscribe();
+        if (this.cryptoData) {
+            this.cryptoData.unsubscribe();
+        }
     };
     return StocksSidebarComponent;
 }());
