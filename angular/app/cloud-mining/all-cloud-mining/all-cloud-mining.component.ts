40ba4969c4bd93609ccdf0ddadb8bd8666221ab3
Robin <ageruuu@gmail.com>
2018-05-23 17:24:45 +0200

    портфель

--- angular/app/cloud-mining/all-cloud-mining/all-cloud-mining.component.ts
+++ angular/app/cloud-mining/all-cloud-mining/all-cloud-mining.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ElementRef } from '@angular/core';
-import { Http } from '@angular/http';
-import { OnChanges } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs/Rx';
-// import { interval } from 'rxjs/Observable/interval';
 import {Router, ActivatedRoute, NavigationEnd} from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { OrderPipe } from '../../order-pipe/ngx-order.pipe';
