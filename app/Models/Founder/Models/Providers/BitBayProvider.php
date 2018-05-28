c22433446f9e6a9ddf0b8ecb3039d379a0fd33d3
Xeror <grides192@gmail.com>
2018-05-28 09:01:56 +0300

    new exchanges

--- app/Models/Founder/Models/Providers/BitBayProvider.php
+++ app/Models/Founder/Models/Providers/BitBayProvider.php
@@ -10,7 +10,9 @@ namespace App\Models\Founder\Models\Providers;
 
 
 use App\Models\Entity\Exchange;
+use App\Models\Entity\ExchangeRate;
 use App\Models\Founder\Models\Connectors\BitBayConnector;
+use App\Models\Founder\Models\Entity\TickerEntity;
 use App\Models\Founder\Models\FounderProvider;
 use App\Models\Founder\Models\Requests\Request;
 use App\Models\Founder\Models\Test\BitBay;
@@ -27,11 +29,6 @@ class BitBayProvider extends FounderProvider
         return $connector;
     }
 
-    public function search(Request $request)
-    {
-        return [];
-    }
-
     public function getExchangeId()
     {
         return 7;
@@ -41,4 +38,48 @@ class BitBayProvider extends FounderProvider
     {
         return new BitBayConnector();
     }
+
+    /**
+     * @param TickerEntity[] $response
+     */
+    public function save($response)
+    {
+        sleep(1);
+        foreach ($response as $ticker) {
+            $exchange = new ExchangeRate();
+            $exchange->value = $ticker->getValue();
+            $exchange->volume = $ticker->getVolume();
+            $exchange->bid = $ticker->getBid();
+            $exchange->ask = $ticker->getAsk();
+            $exchange->currency = $ticker->getCurrency();
+            $exchange->exchangeId = $this->getExchangeId();
+            $exchange->createTime = time();
+            try {
+                $exchange->save();
+            } catch (\Exception $e) {
+
+            }
+        }
+    }
+
+    public function search(Request $request)
+    {
+        $response = $this->getConnector()->search();
+        $result = [];
+        if (!$response) {
+            return $result;
+        }
+
+        $ticker = new TickerEntity();
+        $ticker->setAsk($response->ask);
+        $ticker->setBid($response->bid);
+        $ticker->setVolume($response->volume);
+        $ticker->setValue($response->last);
+        $ticker->setExchangeId($this->getExchangeId());
+        $ticker->setCurrency("BTC/USD");
+        $result[] = $ticker;
+
+        return $result;
+    }
+
 }
