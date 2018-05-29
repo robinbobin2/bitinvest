7975c9c0943146107110a491a593c5f2dee6d49f
Robin <ageruuu@gmail.com>
2018-05-29 16:02:04 +0200

    фильтры в биржах

--- angular/app/review/review-detail/review-detail.component.ts
+++ angular/app/review/review-detail/review-detail.component.ts
@@ -1,92 +1,151 @@
 import { Component, OnInit } from '@angular/core';
-import { ElementRef } from '@angular/core';
-import { Http } from '@angular/http';
-import { OnChanges } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs/Rx';
-// import { interval } from 'rxjs/Observable/interval';
 import {Router, ActivatedRoute, NavigationEnd} from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {CommentsService} from "../../comments.service";
 
 export class News {
-  id: number;
-  title: string;
-  desc: string;
-  created_at:string;
+    id: number;
+    title: string;
+    desc: string;
+    // main: number;
+    category:string;
+    photo: string;
+    created_at:string;
+    comments_count: number;
 
 }
 export class CommentRaw {
-  id: number;
-  author: string;
-  body: string;
-  email: string;
-  commentable_id:number;
-  photo:string;
+    id: number;
+    author: string;
+    body: string;
+    email: string;
+    commentable_id:number;
+    photo:string;
+    created_at;
+}
+export class User {
+    id:number;
+    name: string;
+    email:string;
+    photo_id: number;
+    role_id: number;
 }
 @Component({
-  selector: 'app-review-detail',
-  templateUrl: './review-detail.component.html',
-  styleUrls: ['./review-detail.component.scss']
+    selector: 'app-review-detail',
+    templateUrl: './review-detail.component.html',
+    styleUrls: ['./review-detail.component.scss']
 })
 export class ReviewDetailComponent implements OnInit {
-news: News;
-comments: CommentRaw[] = [];
-  constructor(private http:HttpClient, private router:Router, private route:ActivatedRoute) { 
-    let id = route.snapshot.params['id'];
-    let path = "/reviewraw/"+id;
-    const info = http.get(path);
-  		info.subscribe(response => {
-  			this.news = {
-            id: response['news'][0]['id'],
-          title: response['news'][0]['title'],
-          desc: response['news'][0]['desc'],
-          created_at:response['news'][0]['created_at']
-        }
-        for(let item of response['comments']) {
-          this.comments.push({
-            id: item['id'],
-            email:item['email'],
-          author: item['author'],
-          body: item['body'],
-          commentable_id:item['commentable_id'],
-          photo: item['photo']
-        })
-        }
-        
-  			// console.log(response['news'][0]['id']);
-  			// console.log(this.news);
-     //    console.log(this.comments);
+    news: News;
+    comments: CommentRaw[] = [];
+    submitted = false;
+    commentcount = 0;
+    rating_count = [];
+    user: User;
+    hide = false;
+    constructor(private http:HttpClient, private router:Router, private route:ActivatedRoute, private commentService: CommentsService) {
+        let id = route.snapshot.params['id'];
+        let path = "/reviewraw/"+id;
+        const info = http.get(path);
+        info.subscribe(response => {
+            this.news = {
+                id: response['news'][0]['id'],
+                title: response['news'][0]['title'],
+                desc: response['news'][0]['desc'],
+                created_at:response['news'][0]['created_at'],
+                category:response['category'].name,
+                photo:response['photos'][0].file,
+                comments_count:response['comments_count']
+            }
+            this.commentcount = response['comments_count'];
+            this.comments.push(...response['news'][0]['comments']);
 
-  		});
-}
+            for(let item of response['news'][0]['comments']) {
+                this.rating_count[item['id']] = 0;
+                for (let rating_item of item.rating) {
+                    if (rating_item.positive == 1) {
+                        this.rating_count[item['id']] +=1;
+                    } else {
+                        this.rating_count[item['id']] -=1;
+                    }
+                }
+            }
 
-  ngOnInit() {
-    this.router.events.subscribe((evt) => {
+
+        });
+    }
+
+    ngOnInit() {
+        this.router.events.subscribe((evt) => {
             if (!(evt instanceof NavigationEnd)) {
                 return;
             }
             window.scrollTo(0, 0)
         });
-  }
-  comment = { 
-    'post_id': '',
-    'body': '',
-    'commentable_id': '',
-    'commentable_type': ''
-  }
-  // @ViewChild('f') Form:NgForm;
-  submitComment(form: NgForm, post_id, type) {
-    const headers = new HttpHeaders({'Content-type': 'Application/json '});
-    this.http.post('/storecomment', {
+        const userpath = "/angular/user";
+        const userinfo = this.http.get<User>(userpath);
+        userinfo.subscribe(response => {
+            this.user = {
+                id:response.id,
+                name: response.name,
+                email:response.email,
+                photo_id: response.photo_id,
+                role_id: response.role_id
+
+            };
+        });
+    }
+    comment = {
+        'post_id': '',
+        'body': '',
+        'commentable_id': '',
+        'commentable_type': ''
+    }
+    // @ViewChild('f') Form:NgForm;
+    onVote(comment_id, positive) {
+        this.commentService.addVote(comment_id,positive).subscribe(
+            res =>
+            {
+                console.log(res) ;
+                if(res['error']) {
+                    // code...
+                } else {
+                    if (positive == 1) {
+                        this.rating_count[comment_id] += 1;
+                    } else {
+                        this.rating_count[comment_id] -= 1;
+
+                    }
+                }
+            },
+            error => console.log(error)
+        );
+    }
+    submitComment(form: NgForm, post_id, type) {
+        const headers = new HttpHeaders({'Content-type': 'Application/json '});
+        this.http.post('/storecomment', {
             'post_id': post_id,
             'body': form.value.body,
             'commentable_id': post_id,
             'commentable_type': type
-      }, {headers: headers}).subscribe(
-        (response) => console.log(response),
-        (error) => console.log(error)
-      );
-    // console.log(post_id + " " + form.value.body + " " + type); 
-  }
+        }, {headers: headers}).subscribe(
+            (response) => {
+                this.comments.unshift({
+                    id: response['id'],
+                    email:response['email'],
+                    author: response['author'],
+                    body: response['body'],
+                    commentable_id:response['commentable_id'],
+                    photo: response['photo'],
+                    created_at: response['created_at']
+                })
+                this.submitted = true;
+                this.commentcount=this.commentcount+1;
+            },
+            (error) => console.log(error)
+        );
+        form.reset();
 
+    }
 }
