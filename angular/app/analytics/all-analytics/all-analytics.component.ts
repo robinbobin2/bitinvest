cf55fb1fd463f2453bb51dd0e019da9e6becbb13
Robin <ageruuu@gmail.com>
2018-05-29 20:42:37 +0200

    reviews

--- angular/app/analytics/all-analytics/all-analytics.component.ts
+++ angular/app/analytics/all-analytics/all-analytics.component.ts
@@ -1,96 +1,40 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { OnChanges } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-// import { interval } from 'rxjs/Observable/interval';
-import {Router, ActivatedRoute, NavigationEnd} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MasonryOptions } from 'angular2-masonry';
- 
 
-const scr = 'http://ppql.ru/masonry.js';
-interface Category {
-  id: number;
-  name: string;
-}
+import { OrderPipe } from '../../order-pipe/ngx-order.pipe';
 
-export class NewsRaw {
-  id: number;
-  title: string;
-  desc: string;
-  main: number;
-  category:string;
-  photo: string;
-  created_at:string;
-
-}
 @Component({
-  selector: 'app-all-analytics',
-  templateUrl: './all-analytics.component.html',
-  styleUrls: ['./all-analytics.component.scss']
+    selector: 'app-all-analytics',
+    templateUrl: './all-analytics.component.html',
+    styleUrls: ['./all-analytics.component.scss']
 })
 export class AllAnalyticsComponent implements OnInit {
-
-public myOptions: MasonryOptions = { 
-      transitionDuration: '0' 
-};
-  news_raw: any[];
-	news: NewsRaw[] = [];
-  main_news: NewsRaw[] = [];
-   constructor(private http:HttpClient, private router:Router, private route:ActivatedRoute) { 
-   	let path = "/analyticsraw";
-   	const info = http.get(path);
-   	info.subscribe(response => {
-       // console.log(response['news']);
-       // this.news = response['news'];
-       for (let item of response['news']) {
-         console.log(item.photos[0].file);
-           this.news.push( {
-         id: item.id,
-          title: item.title,
-          desc: item.desc,
-          main: item.main,
-          created_at:item.created_at,
-          category: item.category.name,
-          photo: item.photos[0].file
-
-       });
-         }
-         
-
-       for (let item of response['main_news']) {
-         console.log(item);
-         this.main_news.push( {
-         id: item.id,
-          title: item.title,
-          desc: item.desc,
-          main: item.main,
-          category:item.category.name,
-          created_at:item.created_at,
-          photo: item.photos[0].file
-
-       });
-       }
-   		// this.news.push(response['news']);
-     //   this.main_news.push(response['main_news']);
-   		// console.log(response['news']);
-       // console.log(this.news);
-       // console.log(this.news[0].photo);
-       // console.log(this.main_news);
-   	});
+    news_raw: any[];
+    news= [];
+    main_news = [];
+    info: any;
+    countAll: any;
+    order: string = 'id';
+    reverse: boolean = false;
+    /**
+     * @param {OrderPipe}
+     */
+    constructor(private orderPipe: OrderPipe, private http:HttpClient) {
+        let path = "/analyticsraw";
+        this.info = http.get(path);
 
 
 
-   }
+    }
 
-  ngOnInit() {
-    
-    
-  }
+    ngOnInit() {
 
-  loadMore(id) {
-    this.router.navigate(['/analytics/item', id]);
-  }
-  
+        this.info.map(response => {
+            this.news = response['news'];
+            this.main_news = response['main_news'];
+            this.countAll = this.news.length+this.main_news.length;
+        }).subscribe()
+    }
+
 
 }
--- angular/app/analytics/analytics.component.ts
+++ angular/app/analytics/analytics.component.ts
@@ -1,50 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { OnChanges } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-// import { interval } from 'rxjs/Observable/interval';
-import {Router, ActivatedRoute} from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 interface Categories {
-  id: number;
-  name: string;
-  count: number;
+    id: number;
+    name: string;
+    count: number;
 
 }
-
 @Component({
-  selector: 'app-analytics',
-  templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+    selector: 'app-analytics',
+    templateUrl: './analytics.component.html',
+    styleUrls: ['./analytics.component.scss']
 })
+
 export class AnalyticsComponent implements OnInit {
 
- categories: Categories[] = [];
-  constructor(private http:HttpClient, private router:Router, private route:ActivatedRoute) { 
-    let path = "/categoriesraw/3";
-    const info = http.get(path);
-  		info.subscribe(response => {
-  			for ( let item of response['cats']) {
-  				this.categories.push({
-  				  id: item['id'],
-				  name:item['name'],
-				  count: item['count']
-  			});
-  			}
-  			
-  			console.log(this.categories);
+    categories: Categories[] = [];
+    constructor(private http:HttpClient) {
+        let path = "/categoriesraw/3";
+        const info = http.get(path);
+        info.subscribe(response => {
+            for ( let item of response['cats']) {
+                if(item['count'] > 0) {
+                    this.categories.push({
+                        id: item['id'],
+                        name:item['name'],
+                        count: item['count']
+                    });
+                }
 
-  		});
-}
+            }
 
-  ngOnInit() {
+            console.log(this.categories);
 
-  }
-  loadCat(id) {
-  	this.router.navigate(['/analytics/category', id]);
-    // console.log('snap');
-  
-  }
+        });
+    }
+
+    ngOnInit() {
+
+    }
+
 
 }
