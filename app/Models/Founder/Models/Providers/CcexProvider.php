5e02f03766ab843372889dc7c791c8e62fdd69a4
Xeror <grides192@gmail.com>
2018-05-29 18:33:28 +0300

    new exchanges

--- app/Models/Founder/Models/Providers/CcexProvider.php
+++ app/Models/Founder/Models/Providers/CcexProvider.php
@@ -9,8 +9,11 @@
 namespace App\Models\Founder\Models\Providers;
 
 
+use App\Models\Entity\ExchangeRate;
 use App\Models\Founder\Models\Connectors\CcexConnector;
+use App\Models\Founder\Models\Entity\TickerEntity;
 use App\Models\Founder\Models\FounderProvider;
+use App\Models\Founder\Models\Requests\Request;
 
 class CcexProvider extends FounderProvider
 {
@@ -38,5 +41,49 @@ class CcexProvider extends FounderProvider
     public function isCrypto()
     {
         return true;
+    }
+
+    /**
+     * @param TickerEntity[] $response
+     */
+    public function save($response)
+    {
+        sleep(1);
+        foreach ($response as $ticker) {
+            $exchange = new ExchangeRate();
+            $exchange->value = $ticker->getValue();
+            $exchange->volume = $ticker->getVolume();
+            $exchange->bid = $ticker->getBid();
+            $exchange->ask = $ticker->getAsk();
+            $exchange->currency = $ticker->getCurrency();
+            $exchange->exchangeId = $this->getExchangeId();
+            $exchange->createTime = time();
+            try {
+                $exchange->save();
+            } catch (\Exception $e) {
+
+            }
+        }
+    }
+
+    public function search(Request $request)
+    {
+        $response = $this->getConnector()->search();
+        $result = [];
+        if (!$response) {
+            return $result;
+        }
+        foreach ($response->result as $supplierTicker){
+            $ticker = new TickerEntity();
+            $ticker->setAsk($supplierTicker->Ask);
+            $ticker->setBid($supplierTicker->Bid);
+            $ticker->setVolume($supplierTicker->BaseVolume);
+            $ticker->setValue($supplierTicker->Last);
+            $ticker->setExchangeId($this->getExchangeId());
+            $ticker->setCurrency(strtoupper(str_replace("-", "/", $supplierTicker->MarketName)));
+            $result[] = $ticker;
+        }
+
+        return $result;
     }
 }
