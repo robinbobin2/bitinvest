c1971aa98b5a39b459915d0bffc4d40aae382612
Xeror <grides192@gmail.com>
2018-05-29 16:46:59 +0300

    new exchanges

--- app/Http/Controllers/AngularController.php
+++ app/Http/Controllers/AngularController.php
@@ -14,16 +14,16 @@ use App\User;
 use App\UserPortfolio;
 use App\UserPortfolioType;
 use App\UserPortfollable;
-use File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class AngularController extends Controller
 {
     /**
      * Serve the angular application.
      *
-     * @return \Illuminate\View\View
+     * @return string
      */
     public function serve()
     {
@@ -56,11 +56,9 @@ class AngularController extends Controller
             ];
         }
         $newUser = User::with("Portfolio")->where('id', '=', $user->id)->first();
-        $portfolio_types = UserPortfolioType::all();
+        UserPortfolioType::all();
         $userPortfolio = [];
-        // foreach ($portfolio_types as $portfolio_type) {
-        //     $portfolios = $portfolio_type->portfolios->where('user_id', $newUser->id);
-        // }
+
         foreach ($newUser->portfolio as $portfolio) {
             $userPortfolio['mining'][] = $portfolio->minings;
             $userPortfolio['ico'][] = $portfolio->ico;
@@ -77,7 +75,7 @@ class AngularController extends Controller
         if (!$user) {
             return abort(401);
         }
-        $portfolio_items = UserPortfollable::all()->where('user_portfolio_id', $id);
+        UserPortfollable::all()->where('user_portfolio_id', $id);
         $port_items = UserPortfolio::all()->where('id', $id);
         $items = [];
         $items['ico'] = [];
@@ -90,11 +88,6 @@ class AngularController extends Controller
             $items['crypto'] = $item->crypto;
             $items['stocks'] = $item->stocks;
         }
-        // foreach ($portfolio_items as $portfolio_item) {
-        //     $items['mining'] = CloudMining::where('id', $portfolio_item->user_portfollable_id)->first();
-        //     $ico = IcoProject::all();
-        //     $items['ico'] = $portfolio_item->ico();
-        // }
         return json_encode($items);
     }
 
@@ -122,58 +115,61 @@ class AngularController extends Controller
             ];
         }
         if ($request->user_portfolio_id == 0) {
-            if(UserPortfollable::where('user_portfollable_id', $request->user_portfollable_id)->where('user_portfollable_type', $request->user_portfollable_type)->delete()) {
-            return [
-            'success' => 'Portfolio deleted'
-            ];
+            if (UserPortfollable::where('user_portfollable_id', $request->user_portfollable_id)->where('user_portfollable_type', $request->user_portfollable_type)->delete()) {
+                return [
+                    'success' => 'Portfolio deleted'
+                ];
             }
         } else {
-            if(UserPortfollable::where('user_portfollable_id', $request->user_portfollable_id)->where('user_portfollable_type', $request->user_portfollable_type)->where('user_portfolio_id', $request->user_portfolio_id)->delete()) {
+            if (UserPortfollable::where('user_portfollable_id', $request->user_portfollable_id)->where('user_portfollable_type', $request->user_portfollable_type)->where('user_portfolio_id', $request->user_portfolio_id)->delete()) {
                 return [
-                'success' => 'Portfolio deleted'
-            ];
+                    'success' => 'Portfolio deleted'
+                ];
             }
         }
-        
+
     }
-    public function icoRemovePortfolio($id) 
+
+    public function icoRemovePortfolio($id)
     {
         $user = Auth::user();
-        if(!$user){
+        if (!$user) {
             return [
                 'error' => 'User not loggined'
             ];
         }
-        $portfolio = UserPortfollable::where('user_portfollable_id', $id)->where('user_portfollable_type', 'App\IcoProject')->delete();
+        UserPortfollable::where('user_portfollable_id', $id)->where('user_portfollable_type', 'App\IcoProject')->delete();
         return [
-                'success' => 'Portfolio deleted'
-            ];
+            'success' => 'Portfolio deleted'
+        ];
     }
-    public function cryptoRemovePortfolio($id) 
+
+    public function cryptoRemovePortfolio($id)
     {
         $user = Auth::user();
-        if(!$user){
+        if (!$user) {
             return [
                 'error' => 'User not loggined'
             ];
         }
-        $portfolio = UserPortfollable::where('user_portfollable_id', $id)->where('user_portfollable_type', 'App\CryptoStat')->delete();
+        UserPortfollable::where('user_portfollable_id', $id)->where('user_portfollable_type', 'App\CryptoStat')->delete();
         return [
-                'success' => 'Portfolio deleted'
-            ];
+            'success' => 'Portfolio deleted'
+        ];
     }
-    public function stocksRemovePortfolio($id) 
+
+    public function stocksRemovePortfolio($id)
     {
         $user = Auth::user();
-        if(!$user){
+        if (!$user) {
             return [
                 'error' => 'User not loggined'
             ];
         }
-        $portfolio = UserPortfollable::where('user_portfollable_id', $id)->where('user_portfollable_type', 'App\Stock')->delete();
+        UserPortfollable::where('user_portfollable_id', $id)->where('user_portfollable_type', 'App\Stock')->delete();
         return [
-                'success' => 'Portfolio deleted'
-            ];
+            'success' => 'Portfolio deleted'
+        ];
     }
 
     public function deletePortfolioCat($id)
@@ -194,6 +190,7 @@ class AngularController extends Controller
     {
         return IcoPercent::all()->where('ico_id', '=', $id)->sortByDesc('percent')->values();
     }
+
     public function exchanges()
     {
         $stocks = Stock::all()->toArray();
@@ -203,58 +200,57 @@ class AngularController extends Controller
         }
         return $stocks;
     }
+
     public function exchange($name)
     {
         return Stock::where('name', $name)->with('coins')->withCount('coins')->with('comments')->withCount('comments')->with('categories')->first()->toArray();
     }
 
-    public function search() {
-        if ($_REQUEST['q'] ) {
-            $cloud_mining = CloudMining::where('name', 'LIKE', '%'.$_REQUEST['q'].'%')->get()->toArray();
-            $coins = CryptoStat::where('name', 'LIKE', '%'.$_REQUEST['q'].'%')
-            ->orWhere('symbol', 'LIKE', '%'.$_REQUEST['q'].'%')
-            ->get()
-            ->toArray();
-            $ico = IcoProject::where('name', 'LIKE', '%'.$_REQUEST['q'].'%')->get()->toArray();
-            $stocks = Stock::where('name', 'LIKE', '%'.$_REQUEST['q'].'%')->get()->toArray();
-            $analytics = Analytics::where('title', 'LIKE', '%'.$_REQUEST['q'].'%')->get()->toArray();
-            $interviews = Interview::where('title', 'LIKE', '%'.$_REQUEST['q'].'%')->get()->toArray();
+    public function search()
+    {
+        if ($_REQUEST['q']) {
+            $cloud_mining = CloudMining::where('name', 'LIKE', '%' . $_REQUEST['q'] . '%')->get()->toArray();
+            $coins = CryptoStat::where('name', 'LIKE', '%' . $_REQUEST['q'] . '%')
+                ->orWhere('symbol', 'LIKE', '%' . $_REQUEST['q'] . '%')
+                ->get()
+                ->toArray();
+            $ico = IcoProject::where('name', 'LIKE', '%' . $_REQUEST['q'] . '%')->get()->toArray();
+            $stocks = Stock::where('name', 'LIKE', '%' . $_REQUEST['q'] . '%')->get()->toArray();
+            $analytics = Analytics::where('title', 'LIKE', '%' . $_REQUEST['q'] . '%')->get()->toArray();
+            $interviews = Interview::where('title', 'LIKE', '%' . $_REQUEST['q'] . '%')->get()->toArray();
 
             return array_merge($cloud_mining, $ico, $coins, $stocks, $analytics, $interviews);
-        } else if($_REQUEST['q'] == ''){
-            return ['error'=>'not found'];
         }
+        return ['error' => 'not found'];
     }
-    public function vote(Request $request) {
+
+    public function vote(Request $request)
+    {
+        /** @var User $user */
         $user = Auth::user();
         if (!$user) {
             return abort(401);
         }
         $vote = CommentRating::where('user_id', $user->id)->where('comment_id', $request->comment_id)->first();
         if ($vote) {
-                # code...
             if ($vote->positive != $request->positive) {
                 $vote->delete();
                 return
-                ['success'=>'vote deleted'];
+                    ['success' => 'vote deleted'];
             }
-            if(CommentRating::where('user_id', $user->id)
+            if (CommentRating::where('user_id', $user->id)
                 ->where('comment_id', $request->comment_id)
                 ->where('positive', $request->positive)
                 ->first()) {
 
-                return ['error'=>'Пользователь уже проголосовал'];
+                return ['error' => 'Пользователь уже проголосовал'];
 
-            } else {
-                    
             }
         } else {
             $data = $request->all();
-                    $data['user_id'] = $user->id;
-                    CommentRating::create($data);
-                    return
-                    ['success'=>'voted'];
+            $data['user_id'] = $user->id;
+            CommentRating::create($data);
+            return ['success' => 'voted'];
         }
-
     }
 }
