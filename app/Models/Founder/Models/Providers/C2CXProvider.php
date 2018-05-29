1e330b5c865e922bf5b7ce8da6ea92ccd1ae6575
Xeror <grides192@gmail.com>
2018-05-29 18:01:24 +0300

    new exchanges

--- app/Models/Founder/Models/Providers/C2CXProvider.php
+++ app/Models/Founder/Models/Providers/C2CXProvider.php
@@ -11,6 +11,7 @@ namespace App\Models\Founder\Models\Providers;
 
 use App\Models\Entity\ExchangeRate;
 use App\Models\Founder\Models\Connectors\C2CXConnector;
+use App\Models\Founder\Models\Custom\SupplierLog;
 use App\Models\Founder\Models\Entity\TickerEntity;
 use App\Models\Founder\Models\FounderProvider;
 use App\Models\Founder\Models\Requests\Request;
@@ -49,7 +50,7 @@ class C2CXProvider extends FounderProvider
         $result = $this->getConnector()->search();
 
         foreach ($result as $currency => $supplierTicker){
-            if(!isset($supplierTicker->data)){
+            if(!isset($supplierTicker->data) || !isset($supplierTicker->data->buy)){
                 continue;
             }
             $supplierTicker = $supplierTicker->data;
