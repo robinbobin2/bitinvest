d22c91c3a942cf9f620db7b4c9a340f478957da4
Xeror <grides192@gmail.com>
2018-06-04 16:31:09 +0300

    fix exchanges

--- app/Models/Founder/Models/Providers/CexProvider.php
+++ app/Models/Founder/Models/Providers/CexProvider.php
@@ -9,8 +9,11 @@
 namespace App\Models\Founder\Models\Providers;
 
 
+use App\Models\Entity\ExchangeRate;
 use App\Models\Founder\Models\Connectors\CexConnector;
+use App\Models\Founder\Models\Entity\TickerEntity;
 use App\Models\Founder\Models\FounderProvider;
+use App\Models\Founder\Models\Requests\Request;
 
 class CexProvider extends FounderProvider
 {
@@ -33,5 +36,53 @@ class CexProvider extends FounderProvider
         /** @var CexConnector $connector */
         $connector = parent::getConnector();
         return $connector;
+    }
+
+    /**
+     * @param TickerEntity[] $response
+     */
+    public function save($response)
+    {
+        sleep(1);
+        foreach ($response as $ticker) {
+            $exchange = new ExchangeRate();
+            $exchange->value = $ticker->getValue();
+            $exchange->volume = $ticker->getVolume();
+            $exchange->bid = $ticker->getBid();
+            $exchange->ask = $ticker->getAsk();
+            $exchange->currency = $ticker->getCurrency();
+            $exchange->exchangeId = $this->getExchangeId();
+            $exchange->createTime = time();
+            try {
+                $exchange->save();
+            } catch (\Exception $e) {
+
+            }
+        }
+    }
+
+    public function search(Request $request)
+    {
+        $response = $this->getConnector()->search();
+        $result = [];
+        if (!$response) {
+            return $result;
+        }
+        foreach ($response->data as $supplierTicker){
+
+            if(!$this->validate($supplierTicker->pair)){
+                continue;
+            }
+            $ticker = new TickerEntity();
+            $ticker->setAsk($supplierTicker->ask);
+            $ticker->setBid($supplierTicker->bid);
+            $ticker->setVolume($supplierTicker->volume);
+            $ticker->setValue($supplierTicker->last);
+            $ticker->setExchangeId($this->getExchangeId());
+            $ticker->setCurrency(strtoupper(str_replace(":", "/", $supplierTicker->pair)));
+            $result[] = $ticker;
+        }
+
+        return $result;
     }
 }
