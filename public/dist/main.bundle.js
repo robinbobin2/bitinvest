3ce59dd4c6cf64bc124b723fdfa3d909ae5d2722
Robin <ageruuu@gmail.com>
2018-06-04 16:46:18 +0200

    exchanges

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -2878,7 +2878,7 @@ var Chart2Component = (function () {
             new TradingView.widget({
                 "width": 881,
                 "height": 393,
-                "symbol": "BITSTAMP:" + symbol + "USD",
+                "symbol": "COINBASE:" + symbol + "USD",
                 "interval": "D",
                 "timezone": "Etc/UTC",
                 "theme": "Light",
