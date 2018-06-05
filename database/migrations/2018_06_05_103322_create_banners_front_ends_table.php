9eaffc40685f072d919f345a749a74adffada893
roro <ageruuu@gmail.com>
2018-06-05 10:33:35 +0000

    banners

---
+++ database/migrations/2018_06_05_103322_create_banners_front_ends_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBannersFrontEndsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners_front_ends', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners_front_ends');
+    }
+}
