cb26097ba30d8013c39b31054b4cd6b9e31c7302
Robin <ageruuu@gmail.com>
2018-06-11 17:17:24 +0200

    banners

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -53,17 +53,26 @@ class BannerController extends Controller
             if (($timestamp_start < $now) AND ($timestamp_end > $now)) {
                 echo 'yes';
                 echo "<br>";
+            } else {
+                echo "no";
             }
-            // echo "start";
-            // echo $timestamp_start;
-            // echo "<br>";
-            // echo "end";
-            // echo "<br>";
-            // echo $timestamp_end;
-            // echo "<br>";
-            // echo "now";
-            // echo "<br>";
-            // echo $now;
+            echo "start";
+            echo $timestamp_start;
+            echo "<br>";
+            echo "end";
+            echo "<br>";
+            echo $timestamp_end;
+            echo "<br>";
+            echo "now";
+            echo "<br>";
+            echo $now;
+            echo "<br>";
+            echo "math";
+            echo $timestamp_start-$now;
+            echo "<br>";
+            echo $timestamp_end-$now;
+            echo "<br>";
+            
             echo "<pre>";
             print_r($banners);
                 
