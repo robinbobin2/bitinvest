961b368341ad1b7353891da42b03d276d245ea0e
Robin <ageruuu@gmail.com>
2018-06-11 20:45:11 +0200

    banners

--- angular/app/crypto/crypto.component.ts
+++ angular/app/crypto/crypto.component.ts
@@ -279,6 +279,7 @@ export class CryptoComponent implements OnInit, OnDestroy {
       this.prev = this.dataUsd.last;
       localStorage.removeItem(symbol);
       localStorage.setItem(symbol, JSON.stringify(this.dataUsd));
+
     }).subscribe();
     let infoCryptoPath = "/allcrypto/"+symbol;
     this.infoCrypto = this.http.get<PositionData>(infoCryptoPath).publishReplay(1).refCount();
@@ -303,6 +304,17 @@ export class CryptoComponent implements OnInit, OnDestroy {
               }
           }
       }
+        for(let item of response['categories']) {
+            let newsUrl = "/postsbycat/"+item.id;
+            let newsInfo = this.http.get<any>(newsUrl).publishReplay(1).refCount();
+            newsInfo.subscribe(response => {
+                for(let news_item of response['news']) {
+                    this.news.push(news_item)
+                }
+                this.main_news.push(...response['main_news'])
+                console.log(this.news);
+            });
+        }
       this.commentcount = response['comments_count'];
 
       let newsUrl = "/postsbycat/"+this.data.cat_id_news;
