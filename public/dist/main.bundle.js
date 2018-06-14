4e2fad072c06d58685609ba4b65e190805fb65c5
Robin <ageruuu@gmail.com>
2018-06-14 19:11:20 +0200

    mining filter

--- public/dist/main.bundle.js
+++ public/dist/main.bundle.js
@@ -2833,7 +2833,7 @@ var CryptoAllComponent = (function () {
                 _this.cryptoData = __WEBPACK_IMPORTED_MODULE_1_rxjs_Rx__["a" /* Observable */].interval(1000).concatMap(function () { return _this.StockService.bit$; })
                     .subscribe(function (resp) {
                     _this.resp = resp;
-                    // console.log(this.resp)
+                    console.log(_this.resp);
                     _this.algoFilter = Array.from(new Set(admin.map(function (item) { return item.algo; }))).slice();
                     _this.yearFilter = Array.from(new Set(admin.map(function (item) { return item.year; }))).slice();
                     var _loop_1 = function () {
